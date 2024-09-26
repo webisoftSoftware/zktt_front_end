7b35f9e59e572eab2212a8316189ba4d5c53ce49
jeph.tamuno <jeph@webisoft.com>
2024-09-26 17:12:26 -0400

    chore: added chakra ui

---
+++ src/app/fonts.ts
@@ -0,0 +1,12 @@
+// app/fonts.ts
+import { Plus_Jakarta_Sans } from 'next/font/google'
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-plus-jakarta'
+  });
+
+export const fonts = {
+  plusJakartaSans,
+}
