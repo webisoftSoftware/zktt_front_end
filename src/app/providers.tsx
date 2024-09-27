170892adfbae490da40e8b5d1dd46686c0ac49bc
jeph.tamuno <jeph@webisoft.com>
2024-09-26 21:29:13 -0400

    init: zktt v2

--- src/app/providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "@/chakra/theme";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-	return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
-}
