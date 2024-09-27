41373ea966bcc8858ddb6e206daa9e32fcc35709
turnwol7 <justinb.developer@gmail.com>
2024-09-26 21:07:47 -0400

    feature: added controller integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,51 @@ import VideoSection from "@/components/video-section";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 
+// controller imports
+import { useEffect, useState } from "react";
+import { StarknetProvider } from "./StarknetProvider";
+import { useAccount, useConnect, useDisconnect } from "@starknet-react/core";
+import CartridgeConnector from "@cartridge/connector";
+
+// controller integration
+export function ConnectWallet() {
+    const { connect, connectors } = useConnect();
+    const { disconnect } = useDisconnect();
+    const { address } = useAccount();
+  
+    const connector = connectors[0] as unknown as CartridgeConnector;
+  
+    const [username, setUsername] = useState<string>();
+    useEffect(() => {
+      if (!address) return;
+      connector.username()?.then((n) => setUsername(n));
+    }, [address, connector]);
+  
+    return (
+      <div>
+        {address && (
+          <>
+            <p>Account: {address} </p>
+            {username && <p>Username: {username}</p>}
+          </>
+        )}
+  
+        <button
+          onClick={() => {
+            address ? disconnect() : connect({ connector });
+          }}
+        >
+          {address ? "Disconnect" : "Connect"}
+        </button>
+      </div>
+    );
+  }
+
 export default function Home() {
 	return (
 		<>
+			<StarknetProvider>
+			<ConnectWallet></ConnectWallet>
 			<Header />
 			<VStack spacing={8} align="stretch">
 				<Box as="section" bg="gray.900" color="yellow.400" py={16}>
@@ -19,6 +61,7 @@ export default function Home() {
 				</Box>
 			</VStack>
 			<Footer />
+			</StarknetProvider>
 		</>
 	);
 }
