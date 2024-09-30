7f8fced03cb9c3a4edb1ec45a5f619a9c0e4487d
jeph.tamuno <jeph@webisoft.com>
2024-09-30 19:14:10 -0400

    chore: temp black screen

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,39 +1,23 @@
-'use client'
+"use client";
 
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
-import Header from '@/components/ui/header'
-import Footer from '@/components/ui/footer'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function DefaultLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
-}) {  
+	children: React.ReactNode;
+}) {
+	useEffect(() => {
+		AOS.init({
+			once: true,
+			disable: "phone",
+			duration: 500,
+			easing: "ease-out-cubic",
+		});
+	});
 
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: 'phone',
-      duration: 500,
-      easing: 'ease-out-cubic',
-    })
-  })
-
-  return (
-    <>
-      <Header />
-      
-      <main className="grow">
-
-        {children}
-
-      </main>
-
-      <Footer />
-    </>
-  )
+	return <div className="min-h-screen bg-black">{children}</div>;
 }
--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,22 +1,21 @@
 export const metadata = {
-  title: 'Home - Creative',
-  description: 'Page description',
-}
+	title: "Home - Creative",
+	description: "Page description",
+};
 
-import Hero from '@/components/hero'
-import Carousel from '@/components/carousel'
-import Creatives from '@/components/creatives'
-import Faqs from '@/components/faqs'
-import Cta from '@/components/cta'
+// import Hero from '@/components/hero'
+// import Carousel from '@/components/carousel'
+// import Creatives from '@/components/creatives'
+// import Faqs from '@/components/faqs'
+// import Cta from '@/components/cta'
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Carousel />
-      <Creatives />
-      <Faqs />
-      <Cta />
-    </>
-  )
+	return null;
+	// <>
+	// 	<Hero />
+	//   <Carousel />
+	//   <Creatives />
+	//   <Faqs />
+	//   <Cta /> */
+	// </>
 }
