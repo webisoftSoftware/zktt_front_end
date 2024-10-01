79f90f2d092708454050d5b93c3d541624ef1104
charles bourassa <memmoryhole@gmail.com>
2024-10-01 13:49:37 -0400

    Revert "chore: temp black screen"
    This reverts commit 7f8fced03cb9c3a4edb1ec45a5f619a9c0e4487d.

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,23 +1,39 @@
-"use client";
+'use client'
 
-import { useEffect } from "react";
+import { useEffect } from 'react'
 
-import AOS from "aos";
-import "aos/dist/aos.css";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+import Header from '@/components/ui/header'
+import Footer from '@/components/ui/footer'
 
 export default function DefaultLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
-}) {
-	useEffect(() => {
-		AOS.init({
-			once: true,
-			disable: "phone",
-			duration: 500,
-			easing: "ease-out-cubic",
-		});
-	});
+  children: React.ReactNode
+}) {  
 
-	return <div className="min-h-screen bg-black">{children}</div>;
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 500,
+      easing: 'ease-out-cubic',
+    })
+  })
+
+  return (
+    <>
+      <Header />
+      
+      <main className="grow">
+
+        {children}
+
+      </main>
+
+      <Footer />
+    </>
+  )
 }
--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,21 +1,22 @@
 export const metadata = {
-	title: "Home - Creative",
-	description: "Page description",
-};
+  title: 'Home - Creative',
+  description: 'Page description',
+}
 
-// import Hero from '@/components/hero'
-// import Carousel from '@/components/carousel'
-// import Creatives from '@/components/creatives'
-// import Faqs from '@/components/faqs'
-// import Cta from '@/components/cta'
+import Hero from '@/components/hero'
+import Carousel from '@/components/carousel'
+import Creatives from '@/components/creatives'
+import Faqs from '@/components/faqs'
+import Cta from '@/components/cta'
 
 export default function Home() {
-	return null;
-	// <>
-	// 	<Hero />
-	//   <Carousel />
-	//   <Creatives />
-	//   <Faqs />
-	//   <Cta /> */
-	// </>
+  return (
+    <>
+      <Hero />
+      <Carousel />
+      <Creatives />
+      <Faqs />
+      <Cta />
+    </>
+  )
 }
