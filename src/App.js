94880797db2d29f54a14d898228cf00c3863d4ea
charles bourassa <memmoryhole@gmail.com>
2024-10-04 15:23:07 -0400

    slight change to camera and footer padding

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function Rig(props) {
     ref.current.rotation.y = -scroll.offset * (Math.PI * 2) // Rotate contents
     state.events.update() // Raycasts every frame rather than on pointer-move
     easing.damp3(state.camera.position, [-state.pointer.x * 2, state.pointer.y + 1.5, 10], 0.3, delta) // Move camera
-    state.camera.lookAt(0, 0, 0) // Look at center
+    state.camera.lookAt(0, -0.075, 0) // Look at center
   })
   return <group ref={ref} {...props} />
 }
